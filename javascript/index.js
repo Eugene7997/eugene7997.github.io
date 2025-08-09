7daf6a658320e4060f5c3064acfa1fb958712c07
john <ypoh007@e.ntu.edu.sg>
2025-08-09 15:43:33 +0800

    update

--- javascript/index.js
+++ javascript/index.js
@@ -20,15 +20,6 @@ class Header extends HTMLElement {
       this.innerHTML = `
         <nav>            
             <div class="header">
-                <div class="title-name">Eugene Poh</div>
-                <div class="address">
-                    <h3>🏠 Singapore</h3>
-                </div>
-                <div class="contact">
-                    <a class="no-underline" href="mailto:eugene134@protonmail.com"><span>📧</span><span style="text-decoration: underline;"> eugene134@protonmail.com</span></a>
-                    <a class="no-underline" href="https://www.linkedin.com/in/eugene-poh/"><span>💼</span><span style="text-decoration: underline;"> LinkedIn</span></a>
-                    <a class="no-underline" href="https://github.com/Eugene7997"><span>🐙</span><span style="text-decoration: underline;"> GitHub</span></a>
-                </div>
                 <div class="links">
                     <a href="index.html">Home</a>&nbsp;
                     <a href="background.html">Background</a>&nbsp;
@@ -47,17 +38,83 @@ connectedCallback() {
     this.innerHTML = `    
     <footer>            
         <div class="footer">
-            <p>
-                If you have any comments about the web page, you can check out this link:
-                <a href="website_comments.html">Click me!</a>
-            </p>
-            <p><i>Last modified: 14/05/2025</i></p>
+            <p><i>Last modified: 09/08/2025</i></p>
             <p>The current date is: <current-date></current-date></p>
         </div>
     </footer>     
     `;
     }
 }
+
+class ShowerThoughts extends HTMLElement {
+    connectedCallback() {
+        const thoughts = [
+            "Why do kids need to brush their teeth if they are going to fall out anyway?",
+            "Why are we considered wet during a shower but not when we swim?",
+            "The word \"swims\" is the same upside down.",
+            "Why does cheaper restaurants usually require payment upfront, while expensive ones trust you to pay later?",
+            "The sun is the lowest on the food chain yet it can defeat any animal.",
+            "After a certain point in the night, shouldn't stoplights just function like stop signs, letting drivers pass without waiting for a green light?",
+            "Are Humans are man made?"
+        ];
+
+        this.innerHTML = `
+            <div class="shower-thoughts">
+                <div class="accordion">
+                    <div class="accordion-item">
+                        <button class="accordion-header" aria-expanded="false" aria-controls="thoughts-content">
+                            <span class="accordion-title">Shower Thoughts</span>
+                            <span class="accordion-icon">+</span>
+                        </button>
+                        <div class="accordion-content" id="thoughts-content">
+                            <div class="accordion-body">
+                                <div class="thought-display">${thoughts[0]}</div>
+                            </div>
+                            <button class="next-thought-btn">Tell me more!</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        let currentThought = 0;
+        const thoughtDisplay = this.querySelector('.thought-display');
+        const nextBtn = this.querySelector('.next-thought-btn');
+        const accordionHeader = this.querySelector('.accordion-header');
+        const accordionContent = this.querySelector('.accordion-content');
+        const icon = this.querySelector('.accordion-icon');
+
+        // Accordion functionality
+        accordionHeader.addEventListener('click', () => {
+            const isExpanded = accordionHeader.getAttribute('aria-expanded') === 'true';
+            
+            if (isExpanded) {
+                accordionHeader.setAttribute('aria-expanded', 'false');
+                accordionContent.style.maxHeight = '0px';
+                icon.textContent = '+';
+            } else {
+                accordionHeader.setAttribute('aria-expanded', 'true');
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+                icon.textContent = '−';
+            }
+        });
+
+        // Cycle through thoughts functionality
+        nextBtn.addEventListener('click', () => {
+            currentThought = (currentThought + 1) % thoughts.length;
+            thoughtDisplay.style.opacity = '0';
+            thoughtDisplay.style.transform = 'rotateY(90deg)';
+
+            setTimeout(() => {
+                thoughtDisplay.textContent = thoughts[currentThought];
+                thoughtDisplay.style.opacity = '1';
+                thoughtDisplay.style.transform = 'rotateY(0deg)';
+            }, 300);
+        });
+    }
+}
+
 customElements.define('current-date', CurrentDate);
 customElements.define('main-header', Header);
 customElements.define('main-footer', Footer);
+customElements.define('shower-thoughts', ShowerThoughts);
